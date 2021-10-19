f6b2507f06672349563b7f3452c9c28fc3f5d4fe
rajivharlalka <rajivharlalka009@gmail.com>
2021-10-19 22:26:04 +0530

    supabase setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
-const app = require("express")();
+const router = require("express").Router();
+const {
+  createClient
+} = require("@supabase/supabase-js");
+require('dotenv').config();
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.PUBLIC_ANON_KEY);
+
+router.post("/upload", (req, res) => {})
 
 
 app.listen(process.env.PORT || 4000, () => {
