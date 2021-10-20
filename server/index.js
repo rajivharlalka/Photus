805462045cc3494a9a0d087efd991a40f7e994a8
rajivharlalka <rajivharlalka009@gmail.com>
2021-10-20 10:57:51 +0530

    morgan setup done + file read in route

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
-const router = require("express").Router();
+const express = require("express");
+const fileUpload = require('express-fileupload');
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
 const {
   createClient
 } = require("@supabase/supabase-js");
 require('dotenv').config();
 
+app.use(cors());
+app.use(fileUpload());
+app.use(morgan('dev'));
+
 const supabase = createClient(process.env.SUPABASE_URL, process.env.PUBLIC_ANON_KEY);
 
-router.post("/upload", (req, res) => {})
+app.post("/upload", function (req, res) {
+  const data = req.files.file;
+  console.log(data);
+  res.send();
+
+});
 
 
 app.listen(process.env.PORT || 4000, () => {
--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@mui/material";
+import { useState } from "react";
 
 function App() {
+  const [uploadFile, setUploadFile] = useState(null);
+  const [isUploaded, setIsUploaded] = useState(false);
+  const handleUpload = (e) => {
+    console.log(e.target.files);
+    setIsUploaded(true);
+    setUploadFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const fd = new FormData();
+    fd.append("file", uploadFile);
+    console.log(fd);
+    fetch("http://localhost:4000/upload", {
+      method: "POST",
+      body: fd,
+    })
+      .then((res) => res.json())
+      .then(console.log);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="file" onChange={handleUpload} />
+        {isUploaded === true ? (
+          <input type="submit" value="submit" onClick={handleSubmit} />
+        ) : null}
+      </form>
     </div>
   );
 }
