973eab6f6aea37cc34a82d3a2fef0ddd5f81cf67
rajivharlalka <rajivharlalka009@gmail.com>
2021-10-20 12:15:46 +0530

    debug mode

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const app = require("express")();
 const fileUpload = require('express-fileupload');
-const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
 const {
   createClient
 } = require("@supabase/supabase-js");
 require('dotenv').config();
+const path = require("path");
 
 app.use(cors());
 app.use(fileUpload());
@@ -14,11 +14,35 @@ app.use(morgan('dev'));
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.PUBLIC_ANON_KEY);
 
-app.post("/upload", function (req, res) {
+app.post("/upload", async (req, res) => {
   const data = req.files.file;
+  const fileName = path.parse(data.name).name;
+  const ext = path.parse(data.name).ext;
+  data.name = fileName + "_" + new Date().getTime() + ext;
   console.log(data);
-  res.send();
-
+  const {
+    da,
+    err2
+  } = await supabase.storage.getBucket('photos');
+  if (!err2) {
+    console.log("hello");
+    console.log(da);
+    res.send();
+  } else {
+    console.log("hello1");
+    res.send(err2);
+  }
+  const {
+    response,
+    err
+  } = await supabase.storage.from("photos").upload('/public/index.html', data.data);
+  if (!err) {
+    console.log("hello");
+    res.send(response);
+  } else {
+    "hello1"
+    res.send(err);
+  }
 });
 
 
